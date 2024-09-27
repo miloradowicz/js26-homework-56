574ec69737ebe771a849bef4d1743db12675e5ed
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-09-27 20:45:25 +0600

    Ingredient event hooked

--- src/components/BurgerBuilder/Ingredient/Ingredient.tsx
+++ src/components/BurgerBuilder/Ingredient/Ingredient.tsx
@@ -1,21 +1,23 @@
-import { FC } from 'react';
+import { FC, MouseEventHandler } from 'react';
 import { BuilderIngredient } from '../../../types';
 import './Ingredient.css';
 
 interface IngredientProps {
   ingredient: BuilderIngredient;
   amount: number;
+  onAdd: MouseEventHandler;
+  onDelete: MouseEventHandler;
 }
 
-const Ingredient: FC<IngredientProps> = ({ ingredient, amount }) => (
+const Ingredient: FC<IngredientProps> = ({ ingredient, amount, onAdd, onDelete }) => (
   <li>
-    <button type='button'>
+    <button type='button' onClick={onAdd}>
       <img src={ingredient.img} alt={ingredient.displayName} />
       <span>{ingredient.displayName}</span>
     </button>
     <span>{ingredient.price}</span>
     <span>{amount}</span>
-    <button type='button' aria-label='Delete' />
+    <button type='button' aria-label='Delete' onClick={onDelete} />
   </li>
 );
 
